28c0191e2758eb78f3bb7b7d86e35e1789b8fc26
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-03-29 17:18:02 +0600

    first page + menu

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
+let App = () => {
+  return (
+    <div className="app-grid wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+let Header = () => {
+    return (
+        <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+      </header>
+    );
+}
+
+export default Header;
